c8965a77ccaaa2ca3e42ce316700f17551b6ade6
Jordi Riera <jordi.riera@autodesk.com>
2018-02-02 16:42:30 -0500

    nothing

--- src/manifest.py
+++ src/manifest.py
@@ -1,6 +1,6 @@
 from setuptools_scm import get_version
-version = get_version(root='..', relative_to=__file__)
 
+version = get_version(root='..', relative_to=__file__)
 name = 'nepo'
 description = 'universal url opener'
 url = 'https://github.com/foutoucour/nepo'
