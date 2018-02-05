7020a60d38e1a472949d2e9855c6012e3c43c89f
Jordi Riera <jordi.riera@autodesk.com>
2018-02-05 16:17:08 -0500

    add register

--- src/cli.py
+++ src/cli.py
@@ -6,14 +6,26 @@ from click_didyoumean import DYMMixin
 from click_help_colors import HelpColorsGroup
 
 from src import core
+from src.core import config, build_command
 
-logger = logging.getLogger('voodoo')
+logger = logging.getLogger('nepo')
 logging.basicConfig()
 logger.setLevel(logging.INFO)
 
 
 class AllGroup(DYMMixin, HelpColorsGroup, click.Group):  # pylint: disable=too-many-public-methods
-    pass
+    def __init__(self, name=None, commands=None, **attrs):
+        super(AllGroup, self).__init__(name=name, commands=commands, **attrs)
+        self.dynamic_commands = []
+        for command in config.get_commands():
+            self.add_command(cmd=command)
+            self.dynamic_commands.append(command.name)
+
+    def get_command(self, ctx, cmd_name):
+        if cmd_name in self.dynamic_commands:
+            with config.get() as configs:
+                return build_command(cmd_name, configs[cmd_name]['url'])
+        return super(AllGroup, self).get_command(ctx, cmd_name)
 
 
 @click.group(
@@ -24,6 +36,20 @@ class AllGroup(DYMMixin, HelpColorsGroup, click.Group):  # pylint: disable=too-m
 @click.version_option()
 def entry_point():
     pass
+
+
+@entry_point.command()
+@click.argument('name', required=True)
+@click.argument('url', required=True)
+def register(name, url):
+    """Add an url to the config."""
+    with config.get() as configs:
+        if name in configs:
+            configs.pop(name, None)
+
+        configs[name] = {'url': url}
+
+    config.save(configs)
 
 
 @entry_point.command()
@@ -42,6 +68,18 @@ def github(user='', repo=''):
 def inbox():
     core.open_url("https://inbox.google.com")
 
+# codacy = lambda: core.open_url('https://codacy.com')
+#
+# cmd = click.Command('codacy', callback=codacy)
+#
+# entry_point.add_command(cmd)
+
+
+
+            # entry_point.add_command(cmd)
+
+
+# get_commands_from_config()
 
 if __name__ == '__main__':
     sys.exit(entry_point())
--- src/core.py
+++ src/core.py
@@ -1,6 +1,57 @@
+import json
+import os
+from contextlib import contextmanager
+
 import click
 import crayons
+
 
 def open_url(url):
     click.echo("Opening {}.".format(crayons.white(url, bold=True)))
     click.launch(url)
+
+
+class Config(object):
+    def __init__(self):
+        self.__configs = None
+
+    def save(self, configs):
+        with self.__get_file('w') as config_file:
+            json.dump(configs, config_file)
+
+        self.__configs = None
+
+    @contextmanager
+    def get(self):
+        if self.__configs is None:
+            with self.__get_file() as config_file:
+                self.__configs = json.load(config_file)
+        yield self.__configs
+
+    @contextmanager
+    def __get_file(self, mode='r'):
+        home = os.path.expanduser("~")
+        path = os.path.realpath('{}/.commands.json'.format(home))
+
+        if not os.path.exists(path):
+            with open(path, 'w') as datafile:
+                json.dump({}, datafile)
+
+        with open(path, mode) as datafile:
+            yield datafile
+
+    def get_commands(self):
+        with self.get() as commands:
+            for command, details in commands.items():
+                yield build_command(command, details['url'])
+
+
+config = Config()
+
+
+def build_command(name, url):
+    return click.Command(
+        name,
+        callback=lambda: open_url(url),
+        help='Open {}'.format(url)
+    )
